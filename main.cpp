a089262428c8b9d7c06892dd035be913ae782f65
Padamadam <arybojad@gmail.com>
2023-05-05 15:14:28 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// --------- TASK 1 ---------
+// function which will return given amount of bits with desired shift
+// uint32 GetBitsFromUint32(unit32 inputValue, uint8 bitOffset, uint8bitLength);
+// right shift
+
+//example:
+// uint32 res = GetBitsFromUint32(0xABCDEF12, 4, 12);
+// result is: res = 0x00000EF1;
+
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
+
+uint32 GetBitsFromUint32(uint32 inputValue, uint8 bitOffset, uint8bitLength){
+    return 0;
+}
